fa4b681361858879b0db5ad7b7b9823175e9b4be
gasuto88 <echarlott084@gmail.com>
2024-01-31 15:13:00 +0900

    AudioManager追加

---
+++ Assets/Scripts/AudioManagerScript.cs
@@ -0,0 +1,32 @@
+/*-------------------------------------------------
+* AudioManagerScript.cs
+* 
+* 作成日　2024/ 1/31
+* 更新日　2024/ 1/31
+*
+* 作成者　本木大地
+-------------------------------------------------*/
+using UnityEngine;
+
+public class AudioManagerScript : MonoBehaviour 
+{
+ 
+	#region フィールド変数
+	#endregion
+
+	/// <summary>
+    /// 更新前処理
+    /// </summary>
+	private void Start () 
+	{
+		
+	}
+	
+	/// <summary>
+    /// 更新処理
+    /// </summary>
+	private void Update () 
+	{
+		
+	}
+}
